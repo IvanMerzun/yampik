f88d9e588815d8677a06d56b029f3d04ade4ace1
IvanMerzun <i.merzun@uniyar.ac.ru>
2024-12-22 23:36:13 +0300

    готово вроде

--- leks.analizzz/SemanticAnalyzer.h
+++ leks.analizzz/SemanticAnalyzer.h
@@ -1,17 +1,29 @@
 #pragma once
 #include <fstream>
 #include <string>
-#include <vector>
+#include <unordered_map>
+#include <stdexcept>
 #include "Node.h"
 
 class SemanticAnalyzer
 {
 private:
     std::ofstream outFile; // Файл для записи
-    std::vector<std::string> errors; // Для хранения ошибок
+    string typemain, idret; 
+    struct VariableInfo {
+        std::string type;  // Тип переменной
+    };
+
+    // Таблица символов (множество всех объявленных переменных)
+    std::unordered_map<std::string, VariableInfo> declaredVariables;
 
     // Рекурсивная обработка дерева
     void processNode(const Node& node) {
+
+        if (node.getData() == "Begin") {
+            processBegin(node);
+        }
+
         if (node.getData() == "Descriptions") {
             processDescriptions(node);
         }
@@ -20,11 +32,36 @@ private:
             processOperators(node); // Новый обработчик для операторов
         }
 
+        if (node.getData() == "End") {
+            processEnd(node);
+        }
+
         // Рекурсивно обрабатываем все дочерние узлы
         for (int i = 0; i < node.getChildCount(); ++i) {
             processNode(node.getSon(i));
         }
     }
+
+    void processBegin(const Node& node)
+    {
+        typemain = node.getSon(0).getSon(0).getData();
+    }
+
+    void processEnd(const Node& node)
+    {
+        idret = node.getSon(1).getSon(0).getData();
+
+        // Проверяем, существует ли эта переменная в таблице символов
+        if (declaredVariables.find(idret) == declaredVariables.end()) {
+            throw std::runtime_error("Variable '" + idret + "' used before declaration.");
+        }
+
+        // Проверяем, что тип возвращаемой переменной совпадает с типом функции
+        if (declaredVariables[idret].type != typemain) {
+            throw std::runtime_error("Expected in return " + typemain + ", but got " + declaredVariables[idret].type + ".");
+        }
+    }
+
 
     // Обработка Descriptions
     void processDescriptions(const Node& node) {
@@ -38,14 +75,24 @@ private:
     // Обработка Descr
     void processDescr(const Node& node) {
         std::string type = node.getSon(0).getSon(0).getData(); // Тип переменных (int, float)
-        
+
         std::vector<std::string> variables;
-        //collectVariables(node.getSon(1), variables); // Сбор всех переменных из VarList
         if (node.getSon(1).getData() == "VarList")
         {
             collectVariables(node.getSon(1), variables);
         }
 
+        // Проверяем, что переменные еще не были объявлены
+        for (const auto& var : variables) {
+            if (declaredVariables.find(var) != declaredVariables.end()) {
+                // Ошибка: переменная уже объявлена
+                throw std::runtime_error("Variable '" + var + "' is already declared.");
+            }
+            else {
+                // Добавляем переменную в таблицу символов с её типом
+                declaredVariables[var] = { type };
+            }
+        }
 
         // Формируем строку для постфиксной записи
         std::string result = type; // Начинаем с типа данных
@@ -56,9 +103,9 @@ private:
             }
             result += " DECL"; // Добавляем количество и DECL
         }
-        else 
+        else
         {
-            for (const auto& var : variables) 
+            for (const auto& var : variables)
             {
                 result += " " + var; // Добавляем имена переменных
             }
@@ -70,7 +117,7 @@ private:
     }
 
     // Рекурсивный сбор переменных из VarList
-    void collectVariables(const Node& node, std::vector<std::string>& variables) 
+    void collectVariables(const Node& node, std::vector<std::string>& variables)
     {
         for (int i = 0; i < node.getChildCount(); ++i)
         {
@@ -83,9 +130,9 @@ private:
 
     void processOperators(const Node& node)
     {
-        for (int i = 0; i < node.getChildCount(); ++i) 
+        for (int i = 0; i < node.getChildCount(); ++i)
         {
-            if (node.getSon(i).getData() == "Op") 
+            if (node.getSon(i).getData() == "Op")
             {
                 processOp(node.getSon(i));
             }
@@ -96,18 +143,182 @@ private:
     {
         std::string varName = node.getSon(0).getSon(0).getData();  // Получаем имя переменной (Id)
 
-        std::string exprPostfix = processExpr(node.getSon(2));  // Получаем постфиксную запись для выражения
+        // Извлекаем тип выражения справа от оператора
+        std::string exprType = getExprType(node.getSon(2)); //передаем Expr
+
+        // Проверяем тип переменной слева и справа от оператора
+        if (declaredVariables.find(varName) == declaredVariables.end()) {
+            // Ошибка: переменная используется до объявления
+            throw std::runtime_error("Variable '" + varName + "' used before declaration.");
+        }
+
+        std::string varType = declaredVariables[varName].type;
+
+        // Проверяем, что тип переменной и выражения совпадают
+        if (varType != exprType) {
+            throw std::runtime_error("Type mismatch: variable '" + varName + "' is of type '" + varType +
+                "' but expression is of type '" + exprType + "'.");
+        }
+
+        string exprPostfix = processExpr(node.getSon(2));  // Получаем постфиксную запись для выражения
         string result = varName + " " + exprPostfix + " =";
+
         outFile << result << std::endl;
+
+       
     }
+
+    string getExprType(const Node& node) 
+    {
+        
+        if ( node.getChildCount() == 1)
+        {
+            if (node.getSon(0).getSon(0).getData() == "Const")
+            {
+                std::string value = node.getSon(0).getSon(0).getSon(0).getData();
+
+                // Если в значении есть точка, считаем это float, иначе int
+                if (value.find('.') != std::string::npos)
+                {
+                    return "float";  // Число с плавающей точкой
+                }
+                else {
+                    return "int";  // Целое число
+                }
+            }
+
+            // Если это переменная, то возвращаем её тип из таблицы символов
+            if (node.getSon(0).getSon(0).getData() == "Id") {
+                std::string varName = node.getSon(0).getSon(0).getSon(0).getData();
+                if (declaredVariables.find(varName) != declaredVariables.end()) {
+                    return declaredVariables[varName].type;
+                }
+                else {
+                    throw std::runtime_error("Variable '" + varName + "' used before declaration.");
+                }
+            }
+
+            if (node.getSon(0).getSon(0).getData() == "itof")
+            {
+                string rezu = getExprType(node.getSon(0).getSon(2));
+                if (rezu == "int")
+                {
+                    return "float";
+                }
+                else
+                {
+                    throw std::runtime_error("Expected int in itof.");
+                }
+            }
+
+            if (node.getSon(0).getSon(0).getData() == "ftoi")
+            {
+                string rezu = getExprType(node.getSon(0).getSon(2));
+                if (rezu == "float")
+                {
+                    return "int";
+                }
+                else
+                {
+                    throw std::runtime_error("Expected float in ftoi.");
+                }
+            }
+
+        }
+        //берем тип первого первого при помощи getExprTypeNeed, далее проверяем циклом, чтобы остальные операнды справа были точно такие же, находим хоть один не такой, значит ошибка. 
+
+        else if (node.getChildCount() > 1)
+        {
+
+            string currentType = getExprTypeNeed(node.getSon(0)); // Инициализируем тип первого операнда
+
+            for (int i = 1; i < node.getChildCount(); ++i)
+            {
+                // Пропускаем операторы
+                if (node.getSon(i).getData() == "+" || node.getSon(i).getData() == "-") {
+                    continue;
+                }
+
+                // Обрабатываем SimpleExpr
+                if (node.getSon(i).getData() == "SimpleExpr") {
+                    string operandType = getExprTypeNeed(node.getSon(i));  // Вызов функции для получения типа
+
+                    // Проверяем совместимость типов
+                    if (currentType != operandType) {
+                        throw std::runtime_error("Type mismatch in arithmetic expression.");
+                    }
+                }
+            }
+
+            // Возвращаем итоговый тип выражения
+            return currentType;
+        }
+        
+        
+    }
+
+    string getExprTypeNeed(const Node& node)
+    {
+        if (node.getSon(0).getData() == "Const")
+        {
+            std::string value = node.getSon(0).getSon(0).getData();
+
+            // Если в значении есть точка, считаем это float, иначе int
+            if (value.find('.') != std::string::npos)
+            {
+                return "float";  // Число с плавающей точкой
+            }
+            else {
+                return "int";  // Целое число
+            }
+        }
+
+        // Если это переменная, то возвращаем её тип из таблицы символов
+        if (node.getSon(0).getData() == "Id") {
+            std::string varName = node.getSon(0).getSon(0).getData();
+            if (declaredVariables.find(varName) != declaredVariables.end()) {
+                return declaredVariables[varName].type;
+            }
+            else {
+                throw std::runtime_error("Variable '" + varName + "' used before declaration.");
+            }
+        }
+
+        if (node.getSon(0).getData() == "itof")
+        {
+            string rezu = getExprType(node.getSon(2));
+            if (rezu == "int")
+            {
+                return "float";
+            }
+            else
+            {
+                throw std::runtime_error("Expected int in itof.");
+            }
+        }
+
+        if (node.getSon(0).getData() == "ftoi")
+        {
+            string rezu = getExprType(node.getSon(2));
+            if (rezu == "float")
+            {
+                return "int";
+            }
+            else
+            {
+                throw std::runtime_error("Expected float in ftoi.");
+            }
+        }
+    }
+
 
     string processExpr(const Node& node)
     {
         string rez;
 
-        for (int i = 0; i < node.getChildCount(); ++i) 
+        for (int i = 0; i < node.getChildCount(); ++i)
         {
-            if (node.getSon(i).getData() == "SimpleExpr") 
+            if (node.getSon(i).getData() == "SimpleExpr")
             {
                 rez += " " + processSimpleExpr(node.getSon(i));  // Обрабатываем операнды
             }
@@ -127,15 +338,15 @@ private:
     string processSimpleExpr(const Node& node)
     {
         string result;
-        if (node.getSon(0).getData() == "Const") 
+        if (node.getSon(0).getData() == "Const")
         {
             result += node.getSon(0).getSon(0).getData();  // Константа
         }
-        else if (node.getSon(0).getData() == "Id") 
+        else if (node.getSon(0).getData() == "Id")
         {
             result += node.getSon(0).getSon(0).getData();  // Идентификатор
         }
-        else if (node.getSon(0).getData() == "ftoi" || node.getSon(0).getData() == "itof") 
+        else if (node.getSon(0).getData() == "ftoi" || node.getSon(0).getData() == "itof")
         {
             result += processFunctionCall(node) + "CALL";  // Обрабатываем функцию
         }
@@ -154,6 +365,8 @@ private:
         return result;
     }
 
+
+
 public:
     SemanticAnalyzer(const std::string& outputFileName) {
         outFile.open(outputFileName);
@@ -169,11 +382,15 @@ public:
     }
 
     void analyze(const Node& root) {
-        processNode(root);
-    }
+        try {
+            processNode(root);
 
-    const std::vector<std::string>& getErrors() const {
-        return errors;
+            // Если ошибок нет, записываем постфиксную запись
+            outFile << "Postfix notation successfully generated." << std::endl;
+        }
+        catch (const std::runtime_error& e) {
+            // Если ошибка возникла, выводим ее в файл
+            outFile << "Error: " << e.what() << std::endl;
+        }
     }
 };
-
